84b2c1a0ebf4c2320f3bfcb63deb77d0581b8289
peha1401 <peha1401@student.miun.se>
2016-03-10 14:52:21 +0100

    Tillägg på sidan skapatavling + ny klass tournament

---
+++ DSU_g5/Classes/tournament.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DSU_g5
+{
+    public class tournament
+    {
+        public int id_tournament { get; set; }
+        public string tour_name { get; set; }
+        public string tour_info { get; set; }
+        public DateTime registration_start { get; set; }
+        public DateTime registration_end { get; set; }
+        public string tour_start_time { get; set; }
+        public string tour_end_time { get; set; }
+        public DateTime publ_date_startlists { get; set; }
+        public int contact_person { get; set; }
+        public int gameform { get; set; }
+        public int hole { get; set; }
+    }
+}
